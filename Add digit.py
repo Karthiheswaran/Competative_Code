fec0e73ded3644b8ff61e2fc1fd81aec71545c25
Karthiheswaran <karthikvolleyball16@gmail.com>
2026-01-09 21:30:30 +0530

    Add digit

---
+++ Add digit.py
@@ -0,0 +1,9 @@
+lution(object): 
+    def addDigits(self, num): 
+        while num >= 10: 
+            s = 0 
+            while num > 0: 
+                s += num % 10 
+                num //= 10 
+            num = s 
+        return num
