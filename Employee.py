85a56d050d126bee71beb19b8d487752a310284d
Karthiheswaran <karthikvolleyball16@gmail.com>
2026-01-09 21:44:52 +0530

    Check Employee or Manager

---
+++ Employee.py
@@ -0,0 +1,11 @@
+Class Karthi:
+    def __init__(self):
+        self.employee = "i am employee"
+        self.__manager = "i am manager"
+    def pps(self):
+        print(self.__manager)
+ 
+ 
+info=Karthi()
+print(info.employee)
+infor.pps()
