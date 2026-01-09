a879a075b247c003b57285dcd2bdee54fea1849a
Karthiheswaran <karthikvolleyball16@gmail.com>
2026-01-09 20:29:07 +0530

    Create Area of circle by oops

---
+++ Area of circle by oops.py
@@ -0,0 +1,11 @@
+ class Circle:
+ def __init__(self,radius):
+   self.radius = radius
+
+
+ def area(self):
+   return 3.14* self.radius * self.radius
+
+
+circle1 = Circle(5)
+print("Area:", circle1.area())
